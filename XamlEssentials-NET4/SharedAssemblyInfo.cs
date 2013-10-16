dd3a355622d1a813ea1ac79dc222a43b76eda517
advancedrei <robert@advancedrei.com>
2013-10-16 11:26:32 -0400

    - Updated SharedAssemblyInfo & NuGet file for 3.2 RTM

--- XamlEssentials-NET4/SharedAssemblyInfo.cs
+++ XamlEssentials-NET4/SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("Essential Utilities for XAML applications by Robert McLaws")]
+[assembly: AssemblyDescription("Essential Utilities for XAML applications")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AdvancedREI, LLC.")]
 [assembly: AssemblyProduct("XamlEssentials")]
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.2.0.0")]
+[assembly: AssemblyFileVersion("3.2.0.0")]
